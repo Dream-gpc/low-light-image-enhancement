3509b0857aebbb2abe70aaa659e712cf701f2c69
PcGao <57758908+Dream-gpc@users.noreply.github.com>
2022-12-12 16:14:25 +0800

    Update lime.m

--- TraditionalMethod/lime/lime.m
+++ TraditionalMethod/lime/lime.m
@@ -1,9 +1,3 @@
-% ÂÛÎÄ£º LIME: A Method for Low-light Image Enhancement
-% ×÷Õß£ºXiaojie Guo
-% Á´½Ó£º
-% Author: HSW
-% Date: 2018-04-27
-
 clc;
 close all;
 clear;
@@ -17,18 +11,18 @@ addpath(genpath('BM3D\'));
 % img = imread('timg4.png');
 figure;
 imshow(img, []);
-title('Ô­Í¼Ïñ');
+title('åŸå›¾åƒ');
 Model = 'Normal'; % 'Dehaze' / 'Normal'
 method = 'SA'; 
-denoiseFlag = 1;  % = 0Ê±±íÊ¾²»½øĞĞÔëÉù´¦Àí£» = 1Ê±±íÊ¾½øĞĞÔëÉù´¦Àí
+denoiseFlag = 1;  % = 0æ—¶è¡¨ç¤ºä¸è¿›è¡Œå™ªå£°å¤„ç†ï¼› = 1æ—¶è¡¨ç¤ºè¿›è¡Œå™ªå£°å¤„ç†
 if size(img, 3) == 3
     img_in = im2double(img); 
     img_out = Lime_enhance(img_in, Model, method, 0, 0);     
     figure;
     imshow(img_out, []);
-    title(['ÔöÇ¿½á¹û(', 'Model:', Model, ' | Smooth Method:', method, ')']);
+    title(['å¢å¼ºç»“æœ(', 'Model:', Model, ' | Smooth Method:', method, ')']);
     imwrite(img_out,"C:\code2\LIME\lime\321.png")
 else
-    disp('LIMEÄ£ĞÍ´¦Àí²ÊÉ«Í¼Ïñ');
+    disp('LIMEæ¨¡å‹å¤„ç†å½©è‰²å›¾åƒ');
 end
 
